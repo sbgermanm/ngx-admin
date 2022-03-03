41e8d4550170253e5e1b174a4250849bdc4ccae0
sebastian <sebastian.germanmoreno.contractor@bbva.com>
2022-03-03 00:20:36 -0600

    adding simple https call

--- src/app/pages/tables/smart-table/smart-table.component.ts
+++ src/app/pages/tables/smart-table/smart-table.component.ts
@@ -8,7 +8,7 @@ import { SmartTableData } from '../../../@core/data/smart-table';
   templateUrl: './smart-table.component.html',
   styleUrls: ['./smart-table.component.scss'],
 })
-export class SmartTableComponent implements OnInit{
+export class SmartTableComponent implements OnInit {
 
   settings = {
     add: {
